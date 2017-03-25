b489d30d57d445fd4d784e6ae9a8bfba9fd81cdc
zhouqiulin <zql394@foxmail.com>
2017-03-25 15:06:00 +0800

    路由---导航到英雄详情（start）

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboardComponent';
+
+
 
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    ],
-  declarations: [AppComponent,HeroDetailComponent],
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+
+    ]),
+  ],
+  providers: [
+    HeroService
+  ],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, DashboardComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
